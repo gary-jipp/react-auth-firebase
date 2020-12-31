af594a830efacc4f10247f33be15b3d89cb47b42
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-12-31 09:53:04 -0800

    Update

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+// Put your firebase config here.  Copy and paste from Firebase Console
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
